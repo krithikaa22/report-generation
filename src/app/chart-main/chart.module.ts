1da7375c20c5569120f05d0e71dc21d528b77982
krithikaa22 <kuruthiga22@gmail.com>
2022-12-17 10:20:58 +0530

    pivot table

--- src/app/chart-main/chart.module.ts
+++ src/app/chart-main/chart.module.ts
@@ -1,30 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from '../app-routing.module';
 
 import { ChartComponent } from './chart/chart.component';
 import { AddChartComponent } from './add-chart/add-chart.component';
 import { EditChartComponent } from './edit-chart/edit-chart.component';
 import { ChartsComponent } from './charts/charts.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
 
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import {DataTablesModule} from 'angular-datatables';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import {CommonModule} from "@angular/common"
 import {ChartRouteRoutingModule} from "./chart-route-routing.module"
+import {AngularPivotTableModule} from 'angular-pivot-table';
 
 registerLocaleData(en);
 const materialModules = [
@@ -47,7 +40,8 @@ const materialModules = [
     NzDropDownModule,
     CommonModule,
     NgxChartsModule,
-    ChartRouteRoutingModule
+    ChartRouteRoutingModule,
+    AngularPivotTableModule,
   ],
 })
 export class ChartModule { }
--- src/app/chart-main/charts/charts.component.ts
+++ src/app/chart-main/charts/charts.component.ts
@@ -62,6 +62,37 @@ export class ChartsComponent implements OnInit {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
 
+  configs: any = {
+    rows: 'user',
+    columns: 'book',
+    value: 'price',
+    value_formatter: function(value: any) {
+      return '$' + value;
+    }
+  };
+
+  source= [
+    {'user': 'Deba', 'book': 'Angular'},
+    {'user': 'Deba', 'book': 'Physics'},
+    {'user': 'Aditya', 'book': 'Angular2'},
+    {'user': 'Aditya', 'book': 'Angular3'},
+    {'user': 'Aditya2', 'book': 'Angular4'},
+    {'user': 'Aditya2', 'book': 'Angular'},
+    {'user': 'Aditya3', 'book': 'Angular3'},
+    {'user': 'Aditya4', 'book': 'Angular4'},
+    {'user': 'Aditya', 'book': 'Angular5'},
+    {'user': 'Aditya', 'book': 'Angular3'},
+    {'user': 'Aditya2', 'book': 'Angular2'},
+    {'user': 'Aditya', 'book': 'Angular6'},
+    {'user': 'Aditya', 'book': 'Angular7'},
+    {'user': 'Aditya', 'book': 'Angular8'},
+    {'user': 'Aditya', 'book': 'Angular2'},
+    {'user': 'Aditya', 'book': 'Angula3r'},
+    {'user': 'Aditya', 'book': 'Angular9'},
+    {'user': 'Aditya', 'book': 'Angular7'},
+    {'user': 'Aditya', 'book': 'Angular6'},
+  ];
+
   constructor() {
    
   }
